6435151ecd4bb341bf400f562371824754c54d5e
viviaxenov <viviaxenov@gmail.com>
2018-12-09 14:07:23 +0300

    Added implicit Euler method for ODEs and test script for problem 10-8-1a

---
+++ 10-8-1-a.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import methods.euler_method as em
+
+
+def f(x : np.float64, y : np.float64) -> np.float64:
+	return x + np.cos(y)
+
+x_0 = 1
+y_0 = 30
+x_max = 2
+h = 0.01
+
+
+x, y = em.solve(f, x_0, y_0, h, x_max)
+
+plt.plot(x, y, 'b-')
+plt.grid(True)
+plt.show()
---
+++ methods/euler_method.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def solve(f, x_0 : np.float64, y_0 : np.float64, h : np.float64, 
+		x_max : np.float64) -> (np.float64, np.float64):
+	"""
+	Solves a first-order ODE dy/dx = f(x, y) with implicit Euler method
+	Args:
+		f(float, float) -> float - function in right part
+		x_0, y_0 - initial conditions
+		h - grid step
+		x_max - right boundary of the domain of solution
+	Returns: 
+		(x, y) - tuple of numpy.array - regular grid with step h for x 
+		and values of solution y in those points
+	"""
+	x = np.arange(start=x_0, stop=x_max, step=h)
+	y = np.zeros_like(x)
+
+	y[0] = y_0
+	for i in range(1, x.shape[0]):
+		f_prev = f(x[i - 1], y[i - 1])
+		y_pred = y[i - 1] + h*f_prev
+		y[i] = y[i - 1] + 0.5*h*(f_prev + f(x[i], y_pred))
+	return (x, y)
+
+
+
