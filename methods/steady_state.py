ca1bf3639d5672e40b4c74e78389d020367ac0fc
vivivaxenov <vivivaxenov@gmail.com>
2019-03-15 01:02:05 +0300

    Finished working on steady-state problem

--- methods/steady_state.py
+++ methods/steady_state.py
@@ -40,7 +40,7 @@ class SteadyStateProblem:
         main[1:-1] = -self.q(self.domain[1:-1]) - (tmp[:-1] + tmp[1:])/self.h**2
         upper[1:] = tmp[1:]/self.h**2
         lower[:-1] = tmp[:-1]/self.h**2
-        f[1:-1] = self.f(self.domain[1:-1])
+        f[1:-1] = -self.f(self.domain[1:-1])
 
         # filling left boundary condition
         alpha, beta, gamma = self.left_cond
@@ -55,6 +55,47 @@ class SteadyStateProblem:
         f[-1] = gamma
 
         self.solution = s.solve(upper, main, lower, f)
+        return self.solution
 
+    def get_sample(self, n_samples:int):
+        step = self.n_steps//n_samples
+        return np.stack((self.domain[::step], self.solution[::step]))
+
+
+def get_k_model(k_1: np.float64, k_2: np.float64, x_0: np.float64):
+    def k_model(x):
+        a = np.zeros_like(x)
+        a[x >= x_0] = k_2
+        a[x < x_0] = k_1
+        return a
+    return k_model
+
+
+def get_array_const(val: np.float64):
+
+    def constant(x: np.ndarray):
+        return np.full_like(x, val)
+
+    return constant
+
+
+def get_analytical(left_cond: tuple, right_cond:tuple, k: np.float64, q: np.float64, f: np.float64):
+    alpha_l, beta_l, gamma_l = left_cond
+    alpha_r, beta_r, gamma_r = right_cond
+    lam = np.sqrt(q/k)
+
+    b = np.array([gamma_l -beta_l*f/q, gamma_r - beta_r*f/q])
+    A = np.zeros([2, 2])
+    A[0, 0] = alpha_l*lam + beta_l
+    A[0, 1] = -alpha_l*lam + beta_l
+    A[1, 0] = (alpha_r*lam + beta_r)*np.exp(lam)
+    A[1, 1] = (-alpha_r*lam + beta_r)*np.exp(-lam)
+
+    C = np.linalg.solve(A, b)
+
+    def analytical(x : np.ndarray):
+        return C[0]*np.exp(lam*x) + C[1]*np.exp(-lam*x) + f/q
+
+    return analytical
 
 
--- practical_steady_st.py
+++ practical_steady_st.py
@@ -2,25 +2,98 @@ import numpy as np
 import numpy.linalg
 import matplotlib.pyplot as plt
 
-
 import methods.steady_state as ss
-from methods.shuttle import solve as shsolve
 
-k = lambda x: np.full_like(x, 1.0)
-q = lambda x: np.full_like(x, 1.0)
-f = lambda x: np.full_like(x, 0.0)
+# solving model problem
 
-def get_k_model(k_1, k_2, x_0):
-    def k_model(x):
-        a = np.zeros_like(x)
-        a[x >= x_0] = k_2
-        a[x < x_0] = k_1
-        return a
-    return k_model
 
-problem = ss.SteadyStateProblem((0, 1.0, 0.0), (0, 1.0, 10.0), get_k_model(1.0, 5.0, 0.5), q, f, 2000)
-problem.solve()
+k = ss.get_array_const(1.25)
+q = ss.get_array_const(2.5)
+f = ss.get_array_const(np.cos(0.5))
 
-plt.plot(problem.domain, problem.solution)
-plt.grid()
+l_cond = (k(0.0), -100.0, .0)
+r_cond = (-k(1.0), .0, .0)
+
+#l_cond = (0.0, 1.0, 0.0)
+#r_cond = (0.0, 1.0, 1.0)
+
+analytical = ss.get_analytical(l_cond, r_cond, 1.25, 2.5, np.cos(0.5))
+
+model_problem = ss.SteadyStateProblem(l_cond, r_cond, k, q, f, n_steps=40000)
+model_problem.solve()
+
+fig, ax = plt.subplots(1, 1)
+ax.plot(model_problem.domain, model_problem.solution, 'b-', label='Numeric')
+ax.plot(model_problem.domain, analytical(model_problem.domain), 'r--', label='Analytical')
+ax.set_xlabel('$x$')
+ax.set_ylabel('$u(x)$')
+
+plt.grid(True)
+plt.show()
+
+s = model_problem.get_sample(10)
+err = (s[1] - analytical(s[0]))/s[1]
+
+with np.printoptions(precision=5, linewidth=1000):
+    print('-------------------------- Model problem ------------------------------------------------------------------\n')
+    print('Domain sample:')
+    print(s[0])
+    print('Analytical solution:')
+    print(analytical(s[0]))
+    print('Numeric solution:')
+    print(s[1])
+    print('Relative difference:')
+    print(err)
+    print(f'Error norm = {np.linalg.norm(err, ord=1):.1e}')
+    print('-----------------------------------------------------------------------------------------------------------\n\n')
+
+# Solving problem with varying coeffs
+
+k = lambda x: x**2 + 1
+q = lambda x: x + 2
+f = np.cos
+
+l_cond = (k(0.0), -100.0, .0)
+r_cond = (-k(1.0), .0, .0)
+
+max_prec = 7
+
+ref_problem = ss.SteadyStateProblem(l_cond, r_cond, k, q, f, n_steps=1000000)
+ref_problem.solve()
+ref = ref_problem.get_sample(10)[1]
+
+with np.printoptions(precision=5, linewidth=1000):
+    print('-------------------------- Varying coefs problem ----------------------------------------------------------\n')
+    print('Domain sample:')
+    print(ref_problem.get_sample(10)[0])
+    print('Numeric solution:')
+    print(reft)
+
+errors = []
+steps = []
+
+for n_steps in [10, 50, 100, 500, 1000, 5000, 10000, 50000, 100000, 500000]:
+    pr = ss.SteadyStateProblem(l_cond, r_cond, k, q, f, n_steps=n_steps)
+    pr.solve()
+    s = pr.get_sample(10)[1]
+    err = np.linalg.norm(ref - s, ord=1)
+    step = 1./n_steps
+
+    errors += [err]
+    steps += [step]
+
+errors = np.array(errors)
+steps = np.array(steps)
+
+p = np.polyfit(np.log(steps)[:-1], np.log(errors)[:-1], deg=1)
+line = np.poly1d(p)
+
+fig, ax = plt.subplots(1, 1)
+
+ax.plot(np.log(steps), np.log(errors), 'bs')
+ax.plot(np.log(steps), line(np.log(steps)), 'b--', label=f'$\\frac{{  d(\\log||x - x_r||) }}{{ d(\\log{{h}}) }} \\approx {p[0]:.1f}$')
+ax.set_xlabel(r'$\log{h}$')
+ax.set_xlabel(r'$\log{||u - u_r||_1}$')
+ax.legend()
+ax.grid()
 plt.show()
