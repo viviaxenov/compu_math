1437af4309bd4978f68e633441514e2ed73e6e6e
vivivaxenov <vivivaxenov@gmail.com>
2019-03-17 11:55:22 +0300

    Added analytical solution for problem X-8-11

--- X-8-11.py
+++ X-8-11.py
@@ -2,10 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import methods.euler_method as em
+from methods.helpers import linear_ode_analytical as get_analytical
 
 A = np.array([[0, 1],
               [-100, -101]])
 u_0 = np.array([2, -2])
+
+analytical = get_analytical(A, u_0)
 
 eigval, eigvec = np.linalg.eig(A)
 
@@ -15,22 +18,24 @@ print(f'Stiffness (\\lambda_max/\\lambda_min) = {L:.1e} => system is stiff')
 
 T = 5.5
 n_samples = 10
-n_iter = 25
+t = np.linspace(0, T, n_samples + 1, endpoint=True)
+n_iter = 25:memoryview
 h = T/n_samples/n_iter
 
-coarse = np.zeros([u_0.shape[0], n_samples])
-fine = np.zeros([u_0.shape[0], n_samples])
+coarse = np.zeros([u_0.shape[0], n_samples + 1])
+fine = np.zeros([u_0.shape[0], n_samples + 1])
 
 coarse[:, 0] = u_0
 fine[:, 0] = u_0
 
-for i in range(1, n_samples):
+for i in range(1, n_samples + 1):
     coarse[:, i] = em.solve_lin_syst(coarse[:, i-1], A, h, n_iter, explicit=True)
     fine[:, i] = em.solve_lin_syst(fine[:, i-1], A, h/10, n_iter*10, explicit=True)
 
 fig, ax = plt.subplots(1, 1)
 ax.plot(coarse[0], coarse[1], 'b^', label=f'$h = {h:.1e}$')
 ax.plot(fine[0], fine[1], 'r^', label=f'$h = {h/10:.1e}$')
+ax.plot(analytical(t)[0], analytical(t)[1], 'go--', label='Analytical', markersize=3)
 ax.set_title('Explicit Euler method')
 ax.set_xlabel('$y$')
 ax.set_ylabel('$y\'$')
@@ -38,17 +43,22 @@ ax.grid(True)
 ax.legend()
 plt.show()
 
+err = np.linalg.norm((analytical(t) - fine)/fine, ord=np.inf)
+print(err)
 
-for i in range(1, n_samples):
+
+for i in range(1, n_samples + 1):
     coarse[:, i] = em.solve_lin_syst(coarse[:, i-1], A, h, n_iter)
     fine[:, i] = em.solve_lin_syst(fine[:, i-1], A, h/10, n_iter*10)
 
 fig, ax = plt.subplots(1, 1)
 ax.plot(coarse[0], coarse[1], 'b^')
 ax.plot(fine[0], fine[1], 'r^')
+ax.plot(analytical(t)[0], analytical(t)[1], 'go--', label='Analytical', markersize=3)
 ax.set_title('Implicit Euler method')
 ax.set_xlabel('$y$')
 ax.set_ylabel('$y\'$')
+ax.legend()
 ax.grid(True)
 plt.show()
 
--- methods/helpers.py
+++ methods/helpers.py
@@ -1,4 +1,6 @@
 import numpy as np
+import scipy as sp
+import scipy.linalg
 
 def verify_args(A : np.ndarray, f : np.ndarray) -> bool:
 	shape = A.shape
@@ -8,3 +10,26 @@ def verify_args(A : np.ndarray, f : np.ndarray) -> bool:
 		raise ValueError("Matrix and right part should have same size; Having {0:d} and {1:d}"
 					.format(shape[0], f.shape[0]))
 	return True
+
+
+def linear_ode_analytical(A : np.ndarray, y_0 : np.ndarray):
+	""""
+    Gets analytical solution for ODE system dy/dt = Ay, y(0) = y_0, where A=const
+    Args:
+        A - right part matrix
+        y_0 - initial condition
+    Returns:
+        u - function  : y(t)
+    Warning: works only if A has basis of real eigenvalues. Other cases weren't considered
+    """
+	eigvals, eigvecs = sp.linalg.eig(A)
+
+	def solution(t : np.ndarray) -> np.ndarray :
+		consts = sp.linalg.solve(eigvecs, y_0)
+		l = np.diag(eigvals)
+		times = np.stack((t,t))
+		exps = np.exp(l@times)
+		consts = np.diag(consts)
+		cords = consts@exps
+		return eigvecs@cords
+	return solution
